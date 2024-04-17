92bcf52855f53b9fe43c37c3d05adbc2881840c9
Eternal <eternalnightyeah2@yeah.net>
2024-04-17 20:04:31 +0800

    V0.1.0

--- examples/global_net.rs
+++ examples/global_net.rs
@@ -1,17 +1,12 @@
 use e_utils::parse::{AutoParse as _, ParseResult as _};
 use e_utils::system::env_path_join;
 use e_utils::Result;
-use hikvision::mvs_sdk::HcMvsCoreSdk;
 use hikvision::net_sdk::net::{HcNetCoreSdk, HcnetLevel, NetDvrLocalSdkPath, NetSdkInitCfgType};
 use hikvision::net_sdk::play::HcNetPlayCoreSdk;
 use hikvision::net_sdk::{
     HCNET_CORE_LIB, HCNET_LIBEAY_LIB, HCNET_LIB_ENV, HCNET_SSLEAY_LIB, HCPLAY_CORE_LIB,
 };
-use hikvision::{
-    mvs_sdk::constant::{HCMVS_CAMERA_CONTROL_LIB, HCMVS_LIB_ENV},
-    Lib,
-};
-
+use hikvision::Lib;
 use lazy_static::lazy_static;
 use std::env::set_current_dir;
 use std::sync::{Arc, RwLock};
@@ -38,18 +33,12 @@ fn main() -> Result<()> {
     init_net_sdk(log_folder, HcnetLevel::InfoDE)?;
     HC_NET_CORE_SDK.read().res()?.lib();
     HC_NET_CORE_SDK.write().res()?.free().unwrap();
+    println!("Unload HC_NET_CORE_SDK");
     println!("NET SDK OK");
     HC_NET_PLAY_CORE_SDK.read().res()?.lib();
     HC_NET_PLAY_CORE_SDK.write().res()?.free().unwrap();
+    println!("Unload HC_NET_PLAY_CORE_SDK");
     println!("NET PLAY SDK OK");
-    Ok(())
-}
-
-/// Init SDK
-pub fn init_mvs_sdk() -> Result<()> {
-    set_current_dir(std::env::var(HCMVS_LIB_ENV)?)?;
-    let lib = Lib::new(env_path_join(HCMVS_LIB_ENV, HCMVS_CAMERA_CONTROL_LIB)?);
-    HC_MVS_CORE_SDK.write().res()?.set_lib(lib);
     Ok(())
 }
 
